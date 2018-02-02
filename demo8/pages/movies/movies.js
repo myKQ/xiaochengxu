8ac7ad5dd12c9f1cbd9a331337e05b9bf0998177
zhangqiang <15229203224@163.com>
2018-02-02 16:02:12 +0800

    静态页面及调取豆瓣电影接口完成

--- demo8/pages/movies/movies.js
+++ demo8/pages/movies/movies.js
@@ -1,3 +1,15 @@
 Page({
-  
+  onLoad: function(event) {
+    wx.request({
+      url: "http://t.yushu.im/v2/movie/top250",
+      data:{},
+      method:"GET",
+      header: {
+        'content-type': ''
+      },
+      success: function(res) {
+        console.log(res)
+      }
+    })
+  }
 })
