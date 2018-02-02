d7ecf338341412b97f6d8fb35e0d3eb4743eb733
zhangqiang <15229203224@163.com>
2018-02-02 09:03:33 +0800

    轮播点击事件和两种点击事件的区别

---
+++ demo8/pages/posts/post.js
@@ -0,0 +1,34 @@
+// 这里只能用相对路径
+var postsData = require('../../data/posts-data.js')
+
+Page({
+  data: {
+    
+  },
+  onLoad: function (options) {
+  	// this.data.postList = postsData.postList;
+    this.setData({
+      postList:postsData.postList
+    });
+  },
+  onPostTap:function(event) {
+    // 传递id
+    var postId = event.currentTarget.dataset.postid;
+  	wx.navigateTo({
+      url:"post-detail/post-detail?id=" + postId
+    })
+  },
+  // onSwiperItemTap: function(event) {
+  //   // 传递id
+  //   var postId = event.currentTarget.dataset.postid;
+  //   wx.navigateTo({
+  //     url: "post-detail/post-detail?id=" + postId
+  //   })
+  // },
+  onSwiperTap: function(event) {
+    var postId = event.target.dataset.postid;
+    wx.navigateTo({
+      url: "post-detail/post-detail?id=" + postId
+    })
+  }
+})
