9a176c27dc3961278757ffd774884e19ce8e5303
zhangqiang <15229203224@163.com>
2018-02-05 10:01:47 +0800

    电影星级数据完成

--- demo8/pages/movies/movies.js
+++ demo8/pages/movies/movies.js
@@ -1,3 +1,4 @@
+var util = require('../../utils/util.js')
 var app = getApp();
 Page({
   data: {
@@ -41,6 +42,7 @@ Page({
         title = title.substring(0, 6) + "...";
       }
       var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
         title: title,
         average: subject.rating.average,
         coverageUrl: subject.images.large,
